0981f681177b8c3a266d7b217a6aaa5ddcf84445
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-05-17 12:51:25 +0100

    ongoing project config

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,15 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import { useState } from "preact/hooks";
+import preactLogo from "./assets/preact.svg";
+import "./app.css";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
+        <a href="https://vite.dev" target="_blank"></a>
+        <a href="https://preactjs.com" target="_blank"></a>
       </div>
       <h1>Vite + Preact</h1>
       <div class="card">
@@ -26,7 +21,7 @@ export function App() {
         </p>
       </div>
       <p>
-        Check out{' '}
+        Check out{" "}
         <a
           href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
           target="_blank"
@@ -39,5 +34,5 @@ export function App() {
         Click on the Vite and Preact logos to learn more
       </p>
     </>
-  )
+  );
 }
